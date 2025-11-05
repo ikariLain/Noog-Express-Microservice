86aa16922d8d2df9109c94c56866c8f1d0867640
ikariLain <matheus-torrico@outlook.com>
2025-11-05 13:26:17 +0100

    Add a global client instans

---
+++ src/StreamIOClient.js
@@ -0,0 +1,9 @@
+import { StreamVideoClient } from "@stream-io/video-client"
+
+const apiKey = process.env.STREAMIO_API_KEY
+const secretKey = process.env.STREAMIO_SECRET_KEY
+
+// Initialize a global the Stream Video Client
+const client = new StreamVideoClient(apiKey, secretKey)
+
+export default client
